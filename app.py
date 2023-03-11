e731e8a1bdf6632b4953c67c134830670a16e987
vigo walker <vigopaul05@gmail.com>
2023-03-10 22:41:40 -0300

    Added AI, Version 0.0.2

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request
 from backend import preprocess_text
+import keras
+from keras.models import load_model
+
+# AI
+# define your custom 'TextVectorization' class here
+
+# load the saved model within a CustomObjectScope that includes your custom class
+
+model = load_model('Models\\NLP_Sentence_Encoder_Model\\NLP_Sentence_Encoder_Model')
 
 app = Flask(__name__)
 
@@ -9,6 +18,7 @@ def home():
         text = request.form['text']
         # do something with the text here
         data = preprocess_text(text)
+        output = model.predict(data)
     else:
         text = ''
     return render_template('index.html', title='PubMed 200K', text=text)
